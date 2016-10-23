948d518b81e9ab910db669bf1ba996219dfbfff7
Martin Bondkall Gjerde <Martin Bondkall Gjerde>
2016-10-23 16:23:43 +0200

    pls funk

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
---
+++ js/slideshow.js
@@ -0,0 +1,28 @@
+/* This javascript is based on this tutorial: http://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var i;
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
