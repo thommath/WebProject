c20fcd4e287e4ab01866b30983ad35e61fa8b0c0
Martin Bondkall Gjerde <Martin Bondkall Gjerde>
2016-11-01 14:52:01 +0100

    mappe struktur endring

---
+++ WebPage/js/modal.js
@@ -0,0 +1,12 @@
+/*
+WRITTEN BY: Martin Bondkall Gjerde
+PURPOSE: open and close modal.
+*/
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
