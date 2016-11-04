9fb4d78eeebeb95434deac1655c3463e3ba36d3e
thommath <thomas-lm@vikenfiber.no>
2016-11-04 19:14:02 +0100

    Fixed pangen

--- js/pangen.js
+++ js/pangen.js
@@ -30,11 +30,11 @@ function mouse_up(evt){
 }
 
 function update_color(){
-  color[0] -= 0.1;
-  color[1] -= 0.1;
-  color[2] -= 0.1;
+  color[0] += 0.6;
+  color[1] += 0.6;
+  color[2] += 0.6;
   ctx.strokeStyle = 'rgb(' + parseInt(color[0]) + ',' + parseInt(color[1]) + ',' + parseInt(color[2]) + ')';
-  console.log('rgba(' + color[0] + ',' + color[1] + ',' + color[2] + ')');
+  // console.log('rgba(' + color[0] + ',' + color[1] + ',' + color[2] + ')');
 }
 
 function mouse_move(evt){
@@ -45,6 +45,9 @@ function mouse_move(evt){
 }
 
 function draw(){
+  let nr = 0;for (i in coor_array){for(let n = 1; n < coor_array[i].length; n++){nr++;}}for (i in stroke_array){nr++;}
+
+  color = [255-nr*0.6, 255-nr*0.6, 200-nr*0.6];
   ctx.clearRect(0, 0, 1000, 1000);
   var temp = ctx.lineWidth;
 
@@ -54,6 +57,7 @@ function draw(){
     ctx.moveTo(coor_array[i][0].x, coor_array[i][0].y);
     for(let n = 1; n < coor_array[i].length; n++){
       ctx.lineTo(coor_array[i][n].x, coor_array[i][n].y);
+      update_color();
     }
     ctx.stroke();
   }
@@ -64,8 +68,51 @@ function draw(){
     ctx.lineWidth = stroke_array[0].z;
     for (i in stroke_array){
       ctx.lineTo(stroke_array[i].x, stroke_array[i].y);
+      update_color();
     }
     ctx.stroke();
   }
   ctx.lineWidth = temp;
+}
+
+del_array = [];
+
+function undo(bamsemoms){
+  del_array.push(coor_array.pop());
+  draw();
+}
+
+function redo(bamse){
+  coor_array.push(del_array.pop());
+  draw();
+}
+
+function submit(){
+  temp_arr = [];
+  for (i in coor_array){
+    temp_arr.push({"x":coor_array[i][0].x, "y":coor_array[i][0].y,"f":0,"t":0});
+    for(let n = 1; n < coor_array[i].length; n++){
+      temp_arr.push({"x":coor_array[i][n].x, "y":coor_array[i][n].y,"f":coor_array[i][n].z,"t":0});
+    }
+  }
+  var made_for_database = JSON.stringify(temp_arr);
+
+  var form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", "http://panbot.atwebpages.com/submit.php");
+
+  var hiddenField = document.createElement("input");
+     hiddenField.setAttribute("type", "hidden");
+     hiddenField.setAttribute("name", "name");
+     hiddenField.setAttribute("value", "ygrfht"); //TODO fix it S
+     form.appendChild(hiddenField);
+
+  var hiddenField2 = document.createElement("input");
+     hiddenField2.setAttribute("type", "hidden");
+     hiddenField2.setAttribute("name", "commands");
+     hiddenField2.setAttribute("value", made_for_database);
+     form.appendChild(hiddenField2);
+
+  document.body.appendChild(form);
+  form.submit();
 }
