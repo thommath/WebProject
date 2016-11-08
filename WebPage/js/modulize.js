a1bfd31b82296761ee708959aae4c38f50def0c3
thommath <thomas-lm@vikenfiber.no>
2016-11-08 11:54:43 +0100

    Fixed complete modularity

--- WebPage/js/modulize.js
+++ WebPage/js/modulize.js
@@ -1,24 +1,35 @@
-function loadDoc(container, module, input=[]) {
+/*
+WRITTEN BY: Thomas Lund Mathisen
+PURPOSE: Load basic elements to the webpage
+*/
+
+function loadDoc(container, module, first, input=[]) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var text = this.responseText;
       for(var i = 0; i < input.length; i++){
-        console.log("tolo");
         text = text.replace("{" + i + "}", input[i]);
       }
-     container.innerHTML += text;
+      if(first){
+        container.innerHTML = text + container.innerHTML;
+      }else{
+        container.innerHTML += text;
+      }
     }
   };
-  xhttp.open("GET", "module/" + module + ".html", true);
+  xhttp.open("GET", "/WebProject/WebPage/module/" + module + ".html", true);
   xhttp.send();
 }
 
-
 function setup(){
-  loadDoc(document.getElementById("header"), "header");
-  var body = document.getElementById("box-container");
-  loadDoc(body, "box", ["Digns", "Her er mye fin info"]);
-  loadDoc(body, "box", ["Dumpeditt", "Wiho lorem ipseifmdsk fdsfj dsakd easd"]);
-  loadDoc(body, "box", ["Wioioioioi", "Hola hola hola"]);
+  loadDoc(document.getElementsByTagName("head")[0], "head", false);
+  loadDoc(document.getElementsByTagName("body")[0], "header", true);
+  loadDoc(document.getElementsByTagName("body")[0], "footer", false);
+  var container = document.getElementById("box-container");
+  loadDoc(container, "box", false, ["Digns", "Her er mye fin info"]);
+  loadDoc(container, "box", false, ["Dumpeditt", "Wiho lorem ipseifmdsk fdsfj dsakd easd"]);
+  loadDoc(container, "box", false, ["Wioioioioi", "Hola hola hola"]);
 }
+
+setup();
