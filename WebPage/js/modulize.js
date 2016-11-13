40a0cdd21bd305c68d06ff38c6ba0d79163f90d6
thommath <thomas-lm@vikenfiber.no>
2016-11-13 02:36:35 +0100

    Added comments and fixed some minor details in pangen and modulize, mostly to make it pretty

--- WebPage/js/modulize.js
+++ WebPage/js/modulize.js
@@ -1,11 +1,62 @@
 /*
 WRITTEN BY: Thomas Lund Mathisen
-DATE: 09.11.2016
-PURPOSE: Load basic elements to the webpage
+DATE: 12.11.2016
+PURPOSE: Load basic elements to the webpage and loading elements defined in each file in the right order
+
+How it works:
+First the setup function is called loading the head, header and footer elements.
+When these elements are loaded they are written into the body and head element before they call the callback function basicLoad.
+This function finds out if all basic elements are loaded and then proceed to load each module.
+LoadModules function is called and every module is loaded
+After all modules are loaded the function setup_done is called and modules can safely be loaded into the website
+
+Explaination:
+This is an ineffective way to load each element, but because of the restrictions on other technologies ajax is the only way to load these modules.
+With this js each module will be configured in each html file so this file doesn't need to be altered and therfore it is easier to debug and easier to understand the code.
+This script loads the modules in two parts using callback functions. With the callback functions it can be customized facy in each file, while still being easy to use for simple modules.
+It is a bit slower, but removes all bugs by dependencies that might occur.
+It is quite easy to use but most importantly, flexible, fast and bug free.
+If the user wants to create a module for a module, which is not recomended, it is possible through the callback function option.
+
+How to use:
+In each html file this file should be included so it loads every default module. Do this by adding this absolute path to the file: <script src="/martiwes/js/modulize.js" charset="utf-8"></script>
+You are good to go!
+
+To load a module you simply make a script after loading this where you override the loadModules function and call loadDoc(container, module) where you send an element as container and a string with a module name.
+if the container is body you are good to go, but else you need to append the container after it is loaded by overriding the setup_done function and adding the container as a child to the parent element.
+
+Example:
+  var container = document.createElement("section");
+
+  //@Override
+  function loadModules(){
+    loadDoc(container, "box", false, ["Digns", "Her er mye fin info"]);
+    loadDoc(container, "box", false, ["Dumpeditt", "Wiho lorem ipseifmdsk fdsfj dsakd easd"]);
+    loadDoc(container, "box", false, ["Wioioioioi", "Hola hola hola"]);
+  }
+
+  //@Override
+  function setup_done(){
+    document.getElementById("box-container").appendChild(container);
+  }
+
+The module box contains two textfields marked with {0} and {1}. These will be replaced with the text in the array.
+
+
+The loadModules function can take in 5 parameters, container, module, first, input and callback. The first 4 is pretty basic, but the callback is where it gets exciting, With the callback function the sky is the limit. You can make your own callback function for each element telling it to do whatever you want. The most conveniant thing you can do is to load another module that dependes on the first, it is not recomended but possible! Be creative!
+
+All the functions can be overridden, but we recomend that you don't alter the basic methods.
+
+*/
+
+
+
+/*
+Name: basicLoad
+Purpouse: Keeping track of how many basic modules that hasn't been loaded and start the module loading process when done.
 */
 
 let basicCounter = 0;
-
 function basicLoad(){
   basicCounter -= 1;
   if(basicCounter == 0){
@@ -14,6 +65,12 @@ function basicLoad(){
     moduleLoaded();
   }
 }
+
+
+/*
+Name: moduleLoaded
+Purpouse: Keeping track of how many custom modules that hasn't been loaded and call setup_done when done. All pages should have let it rain so this will also be called
+*/
 
 let moduleCounter = 1;
 function moduleLoaded(){
@@ -24,7 +81,18 @@ function moduleLoaded(){
   }
 }
 
-function loadDoc(container, module, first, input=[], callback=moduleLoaded) {
+
+/*
+Name: loadDoc
+Purpouse: Loading and adding a module to a container
+Explaination:
+First tells the counting functions what kind of module it is if no custom callback.
+Then it makes an ajax file request for the right module
+When the file is loaded it will insert the custom text before it insterts the module into the container
+Then it calls the callback function before it is done
+*/
+
+function loadDoc(container, module, first=false, input=[], callback=moduleLoaded) {
   if(callback == basicLoad){
     basicCounter += 1;
   }else if(callback == moduleLoaded){
@@ -55,11 +123,22 @@ function loadDoc(container, module, first, input=[], callback=moduleLoaded) {
   xhttp.send();
 }
 
+/*
+Name: setup
+Purpouse: Load basic modules
+*/
+
 function setup(){
   loadDoc(document.head, "head", true, [], basicLoad);
   loadDoc(document.body, "header", true, [], basicLoad);
   loadDoc(document.body, "footer", false, [], basicLoad);
 }
+
+
+/*
+Name: remove_loading_elements
+Purpouse: if the site has any loading screen elements these will be removed
+*/
 
 function remove_loading_elements(){
   let del = document.getElementsByClassName("delete");
@@ -68,6 +147,17 @@ function remove_loading_elements(){
   }
 }
 
+
+/*
+functions that might be overwritten in the html file
+*/
+
 function setup_done(){}
 function loadModules(){}
+
+
+/*
+Calls the setup function to get the show started
+*/
+
 setup();
--- WebPage/js/pangen.js
+++ WebPage/js/pangen.js
@@ -1,11 +1,40 @@
-coor_array = [];
-stroke_array = [];
-let ctx;
+/*
+Name: Maria Ivashentseva
+Purpouse: Adding functionality to the PanGen canvas element.
 
+Requirements:
+The user should be able to draw lines on the canvas to get an image up.
+The drawing should be sent to the panbot server as a list of commands on the format [{x: , y: , f: , t: }, ...] as a json string with a post request.
+
+How it works:
+When the user moves the mouse over the pangen the mouse_move will be triggered. This function will check if the mouse button is down and add a line to the canvas if the line long enough to minimize number of commands required.
+When the user clicks the mouse button down it will start a new line in a temp array called stroke_array and it will remeber that the button is down.
+When the user releases the button the mouse_up will be triggered and the stroke will be saved as an array in coor_array
+Each time one of these function is called the draw function is called updating the canvas with the new strokes.
+
+*/
+
+/*
+Creating global variables
+*/
+let coor_array = [];
+let stroke_array = [];
+let del_array = [];
+let ctx;
+let down = false;
+let color = [255, 255, 200];
+
+/*
+Logging the stroke elements for debugging purpouses
+*/
 setInterval(function () {
   console.log(coor_array);
 }, 5000)
 
+/*
+Name: setup_pangen
+Purpouse: Get elements and set default values + draw a greeting message on the canvas
+*/
 function setup_pangen(){
   ctx = document.getElementById('canvas').getContext('2d');
   ctx.font="50px Georgia";
@@ -16,20 +45,23 @@ function setup_pangen(){
   console.log("Canvas setup done");
 }
 
-var down=false;
-var color = [255, 255, 200];
 
-function thickness(elem){
-  document.getElementById("thickness").innerHTML=elem.value;
-  ctx.lineWidth = elem.value;
-}
+/*
+Name: mouse_down
+Purpouse: remember that the mouse button is pressed and save the first element in the temp array relative to the canvas.
+*/
 
 function mouse_down(evt){
   down = true;
   width = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-  height = document.getElementById('heightHeader').offsetHeight + 74;
-  stroke_array.push({x:evt.clientX-(width/2-250), y:evt.clientY-height, z:ctx.lineWidth});
+  height = document.getElementById('heightHeader').offsetHeight + 74;//Compensate for header height and title height
+  stroke_array.push({x:evt.clientX-(width/2-250), y:evt.clientY-height, z:ctx.lineWidth});//Calculate with by dividing the screen in two and subtracting half canvas size
 }
+
+/*
+Name: mouse_up
+Purpouse: remember mouse button is released, pushing the last stroke to the array if it is a line and not just a point before clearing the temp array
+*/
 
 function mouse_up(evt){
   down = false;
@@ -39,29 +71,49 @@ function mouse_up(evt){
   stroke_array = [];
 }
 
+/*
+Name: update_color
+Purpouse: Make the color a bit lighter for each time the function is called
+*/
+
 function update_color(){
   color[0] += 0.6;
-  color[1] += 0.6;
-  color[2] += 0.6;
+  color[1] += 0.7;
+  color[2] += 0.5;
   ctx.strokeStyle = 'rgb(' + parseInt(color[0]) + ',' + parseInt(color[1]) + ',' + parseInt(color[2]) + ')';
 }
 
+/*
+Name: mouse_move
+Purpouse: Save the current relative position if the mouse is pressed and long enough away from last point
+*/
+
 function mouse_move(evt){
-  if(down==true && Math.pow(stroke_array[stroke_array.length-1].x-evt.clientX-200, 2)+Math.pow(stroke_array[stroke_array.length-1].y-evt.clientY-180, 2)>1000/(ctx.lineWidth)){
-    width = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-    height = document.getElementById('heightHeader').offsetHeight + 74;
+  width = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+  height = document.getElementById('heightHeader').offsetHeight + 74;
+  if(down==true && Math.pow(stroke_array[stroke_array.length-1].x-evt.clientX-(width/2-250), 2)+Math.pow(stroke_array[stroke_array.length-1].y-evt.clientY-height, 2)>1000/(ctx.lineWidth)){
     stroke_array.push({x:evt.clientX-(width/2-250), y:evt.clientY-height, z:ctx.lineWidth});
     draw();
   }
 }
 
+/*
+Name: draw
+Purpouse: Draw each line on the canvas with the right width and color, including the temp line
+*/
+
 function draw(){
+  //Counting the length of each stroke in coor array and stroke array to Calculate the starting color for the pancake
   let nr = 0;for (i in coor_array){for(let n = 1; n < coor_array[i].length; n++){nr++;}}for (i in stroke_array){nr++;}
 
+  //Setting start color and empyting the canvas
   color = [255-nr*0.6, 255-nr*0.6, 200-nr*0.6];
   ctx.clearRect(0, 0, 1000, 1000);
+
+  //Saving the line width
   var temp = ctx.lineWidth;
 
+  //Looping through each line and drawing them as one stroke each to maximize design and minimize resources needed
   for (i in coor_array){
     ctx.beginPath();
     ctx.lineWidth = coor_array[i][0].z;
@@ -73,6 +125,7 @@ function draw(){
     ctx.stroke();
   }
 
+  //Drawing the temp line if there is one
   if(stroke_array.length > 1){
     ctx.beginPath();
     ctx.moveTo(stroke_array[0].x, stroke_array[0].y);
@@ -83,22 +136,44 @@ function draw(){
     }
     ctx.stroke();
   }
+
+  //Setting back the line width
   ctx.lineWidth = temp;
 }
 
-del_array = [];
 
+/*
+Name: undo
+Purpouse: Remove the last element in coor_array and adding it to a list of deleted strokes
+*/
 function undo(bamsemoms){
   del_array.push(coor_array.pop());
   draw();
 }
 
+/*
+Name: redo
+Purpouse: Remove the last element in deleted strokes and adding to coor_array
+*/
 function redo(bamse){
   coor_array.push(del_array.pop());
   draw();
 }
+/*
+Name: thickness
+Purpouse: update the thickness of the brush when the slider is changed
+*/
+function thickness(elem){
+  document.getElementById("thickness").innerHTML=elem.value;
+  ctx.lineWidth = elem.value;
+}
 
+/*
+Name: submit
+Purpouse: Converting the data to the right format and send them to the server
+*/
 function submit(){
+  //Adding each stroke to temp_arr on the right format, only points and how thick the stroke is (0 for only moving)
   temp_arr = [];
   for (i in coor_array){
     temp_arr.push({"x":coor_array[i][0].x, "y":coor_array[i][0].y,"f":0,"t":0});
@@ -106,24 +181,29 @@ function submit(){
       temp_arr.push({"x":coor_array[i][n].x, "y":coor_array[i][n].y,"f":coor_array[i][n].z,"t":0});
     }
   }
+  //Convert commands to json string
   var made_for_database = JSON.stringify(temp_arr);
 
+  //Creating a form to send to the server
   var form = document.createElement("form");
   form.setAttribute("method", "post");
   form.setAttribute("action", "http://panbot.atwebpages.com/submit.php");
 
+  //Adding name to the beautful drawing
   var hiddenField = document.createElement("input");
      hiddenField.setAttribute("type", "hidden");
      hiddenField.setAttribute("name", "name");
      hiddenField.setAttribute("value", "ygrfht"); //TODO fix it S
      form.appendChild(hiddenField);
 
+  //Adding the json to the form
   var hiddenField2 = document.createElement("input");
      hiddenField2.setAttribute("type", "hidden");
      hiddenField2.setAttribute("name", "commands");
      hiddenField2.setAttribute("value", made_for_database);
      form.appendChild(hiddenField2);
 
+  //Append and submit the form 
   document.body.appendChild(form);
   form.submit();
 }
